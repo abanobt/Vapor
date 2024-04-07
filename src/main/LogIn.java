8e24380d26081bc8a0aa9a63953a5871a530396b
abanobt <145419255+abanobt@users.noreply.github.com>
2024-04-06 22:40:58 -0400

    Create LogIn screen

---
+++ src/main/LogIn.java
@@ -0,0 +1,78 @@
+package main;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.util.Arrays;
+
+public class LogIn extends JPanel {
+    private static final PercentConstraints ICON_BOUNDS =  new PercentConstraints(.3f, .225f, .1f, .1f);
+    private static final PercentConstraints LOG_IN_LABEL_BOUNDS =  new PercentConstraints(.3f, .225f, .4f, .1f);
+    private static final PercentConstraints USERNAME_FIELD_TITLE_BOUNDS =  new PercentConstraints(.3f,.355f, .4f, .02f);
+    private static final PercentConstraints USERNAME_FIELD_BOUNDS =  new PercentConstraints(.3f,.375f, .4f, .1f);
+    private static final PercentConstraints PASSWORD_FIELD_TITLE_BOUNDS =  new PercentConstraints(.3f,.505f, .4f, .02f);
+    private static final PercentConstraints PASSWORD_FIELD_BOUNDS =  new PercentConstraints(.3f,.525f, .4f, .1f);
+    private static final PercentConstraints LOG_IN_BUTTON_BOUNDS =  new PercentConstraints(.3f, .675f, .4f, .1f);
+
+    private final JTextField usernameField;
+    private final JPasswordField passwordField;
+
+    public LogIn() {
+        setLayout(new PercentLayout());
+
+        Font font = getFont().deriveFont(35f);
+
+        add(new IconLabel(VaporApp.ICON, true), ICON_BOUNDS);
+        add(new JLabel("Log In", SwingConstants.CENTER) {{
+            setFont(font);
+        }}, LOG_IN_LABEL_BOUNDS);
+        
+        add(new JLabel("Username", SwingConstants.LEFT), USERNAME_FIELD_TITLE_BOUNDS);
+        add(usernameField = new JTextField() {{
+            setFont(font);
+        }}, USERNAME_FIELD_BOUNDS);
+        
+        add(new JLabel("Password", SwingConstants.LEFT), PASSWORD_FIELD_TITLE_BOUNDS);
+        add(passwordField = new JPasswordField() {{
+            setFont(font);
+        }}, PASSWORD_FIELD_BOUNDS);
+        
+        add(new JButton("Log In"){{
+            addActionListener(LogIn.this::onLogIn);
+            setBackground(Color.BLACK);
+            setForeground(Color.WHITE);
+            setFont(font);
+        }}, LOG_IN_BUTTON_BOUNDS);
+    }
+
+    private void onLogIn(ActionEvent e) {
+        String username = usernameField.getText();
+        char[] password = passwordField.getPassword();
+        if (username.length() < 1) {
+            JOptionPane.showMessageDialog(VaporApp.APP_SINGLETON, "Username required", "Log In Failed", JOptionPane.ERROR_MESSAGE);
+            passwordField.setText("");
+        } else if (password.length < 1) {
+            JOptionPane.showMessageDialog(VaporApp.APP_SINGLETON, "Password required", "Log In Failed", JOptionPane.ERROR_MESSAGE);
+        } else if (validateLogIn(username, password)) {
+            VaporApp.APP_SINGLETON.loggedIn();
+        } else {
+            JOptionPane.showMessageDialog(VaporApp.APP_SINGLETON,"Invalid username and/or password", "Log In Failed", JOptionPane.ERROR_MESSAGE);
+            passwordField.setText("");
+        }
+        
+        // Clear password arr for security
+        Arrays.fill(password, (char)0);
+    }
+
+    private boolean validateLogIn(String username, char[] password) {
+        // TODO: validate username and password
+        return false;
+    }
+}
