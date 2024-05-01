5797f2d70fb30e4d2da11c0d68829e48aff8b30c
abanobt <145419255+abanobt@users.noreply.github.com>
2024-04-30 22:34:55 -0400

    added addTab method to remove repetead code

--- src/main/MainUI.java
+++ src/main/MainUI.java
@@ -3,55 +3,23 @@ package main;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import java.awt.Color;
 import java.awt.event.KeyEvent;
 
 public class MainUI extends JTabbedPane {
+    private int tabIndex = 0;
+
     public MainUI() {
-//        setLayout(null);
-        addTab("Store", new ImageIcon("res/store_icon.png"),
-            new JPanel() {{
-                setBackground(Color.RED);
-            }}
-        );
-        setToolTipTextAt(0, "Store (Ctrl + S)");
-        setMnemonicAt(0, KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_S);
-
-        addTab("Library", new ImageIcon("res/store_icon.png"),
-            new JPanel() {{
-                setBackground(Color.BLUE);
-            }}
-        );
-        setToolTipTextAt(1, "Library (Ctrl + L)");
-        setMnemonicAt(1, KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_L);
-
-        addTab("Friends", new ImageIcon("res/store_icon.png"),
-            new JPanel() {{
-                setBackground(Color.GREEN);
-            }}
-        );
-        setToolTipTextAt(2, "Friends (Ctrl + F)");
-        setMnemonicAt(2, KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_F);
-
-        addTab("Profile", new ImageIcon("res/store_icon.png"),
-            new JPanel() {{
-                setBackground(Color.YELLOW);
-            }}
-        );
-        setToolTipTextAt(3, "Profile (Ctrl + P)");
-        setMnemonicAt(3, KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_P);
-
-        addTab("Wishlist", new ImageIcon("res/store_icon.png"),
-            new JPanel() {{
-                setBackground(Color.ORANGE);
-            }}
-        );
-        setToolTipTextAt(4, "Wishlist (Ctrl + W)");
-        setMnemonicAt(4, KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_W);
-
+        addTab("Store", StoreUI.STORE_ICON, new StoreUI(), "Store (Ctrl + S)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_S);
+        addTab("Library", StoreUI.STORE_ICON, new JPanel(), "Library (Ctrl + L)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_L);
+        addTab("Friends", StoreUI.STORE_ICON, new JPanel(), "Friends (Ctrl + F)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_F);
+        addTab("Profile", StoreUI.STORE_ICON, new JPanel(), "Profile (Ctrl + P)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_P);
+        addTab("Wishlist", StoreUI.STORE_ICON, new JPanel(), "Wishlist (Ctrl + W)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_W);
     }
 
-    private void addTab() {
-
+    private void addTab(String title, ImageIcon icon, JPanel ui, String tooltip, int mnemonic) {
+        int i = tabIndex++;
+        addTab(title, icon, ui);
+        setToolTipTextAt(i, tooltip);
+        setMnemonicAt(i, mnemonic);
     }
 }
