d1f2bc865910eca65f746bf76ba0e9dbddd0da6c
abanobt <145419255+abanobt@users.noreply.github.com>
2024-05-02 16:30:29 -0400

    Update MainUI

--- src/main/MainUI.java
+++ src/main/MainUI.java
@@ -8,12 +8,15 @@ import java.awt.event.KeyEvent;
 public class MainUI extends JTabbedPane {
     private int tabIndex = 0;
 
+    private final StoreUI storeUI;
+    private final WishlistUI wishlistUI;
+
     public MainUI() {
-        addTab("Store", StoreUI.STORE_ICON, new StoreUI(), "Store (Ctrl + S)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_S);
-        addTab("Library", StoreUI.STORE_ICON, new JPanel(), "Library (Ctrl + L)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_L);
-        addTab("Friends", StoreUI.STORE_ICON, new JPanel(), "Friends (Ctrl + F)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_F);
-        addTab("Profile", StoreUI.STORE_ICON, new JPanel(), "Profile (Ctrl + P)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_P);
-        addTab("Wishlist", StoreUI.STORE_ICON, new JPanel(), "Wishlist (Ctrl + W)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_W);
+        addTab("Store", StoreUI.STORE_ICON, storeUI = new StoreUI(), "Store (Ctrl + S)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_S);
+        addTab("Wishlist", StoreUI.STORE_ICON, wishlistUI = new WishlistUI(), "Wishlist (Ctrl + W)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_W);
+//        addTab("Library", StoreUI.STORE_ICON, new LibraryUI(), "Library (Ctrl + L)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_L);
+//        addTab("Friends", StoreUI.STORE_ICON, new FriendsUI(), "Friends (Ctrl + F)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_F);
+//        addTab("Profile", StoreUI.STORE_ICON, new ProfileUI(), "Profile (Ctrl + P)", KeyEvent.CTRL_DOWN_MASK | KeyEvent.VK_P);
     }
 
     private void addTab(String title, ImageIcon icon, JPanel ui, String tooltip, int mnemonic) {
@@ -21,5 +24,9 @@ public class MainUI extends JTabbedPane {
         addTab(title, icon, ui);
         setToolTipTextAt(i, tooltip);
         setMnemonicAt(i, mnemonic);
+    }
+
+    public void refreshWishlist() {
+        wishlistUI.refresh();
     }
 }
