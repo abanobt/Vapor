0d86884fbabc34e0c11f77bbd2368b922385d1fd
abanobt <145419255+abanobt@users.noreply.github.com>
2024-05-03 14:06:41 -0400

    Create SampleData class
    Class that will be temporarily used to provided sample data values until sql calls are implemented

---
+++ src/ui/SampleData.java
@@ -0,0 +1,17 @@
+package ui;
+
+/**
+ * Sample data class, remove once SQL functionality is added
+ */
+public class SampleData {
+    public static final String[] TITLES = {"Minecraft", "Valorant", "Counter Strike", "Hearts of Iron"};
+    public static final String[] DEVELOPERS = {"Mojang", "Riot", "Valve", "Paradox"};
+    public static final String[] PUBLISHERS = {"Microsoft", "Riot", "Valve", "Paradox"};
+    public static final String[] GENRE = {"Sandbox", "FPS", "FPS", "Grand Strategy"};
+    public static final String[] TAGS = {"Sandbox", "FPS", "FPS", "Grand Strategy"};
+    public static final String[] RELEASE_DATE = {"01/02/2009", "03/04/2021", "05/06/2005", "07/08/2011"};
+    public static final String[] DESCRIPTION = {"01/02/2009", "03/04/2021", "05/06/2005", "07/08/2011"};
+    public static final float[] PRICE = {29.99f, 0.00f, 59.99f, 39.99f};
+    public static final String[] PLATFORMS = {"Windows, Mac", "Windows", "Windows", "Windows, Linux"};
+    public static final float[] AVG_RATING = {9.2f, 6.3f, 4.2f, 9.0f};
+}
