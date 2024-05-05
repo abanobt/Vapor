63db19a72889853508f603f2663150e10d6b3315
chris-k-njit <ck378@njit.edu>
2024-05-05 13:00:02 -0400

    Wishlistgamelabel file updated.

--- src/label/WishlistGameLabel.java
+++ src/label/WishlistGameLabel.java
@@ -1,55 +1,61 @@
 package label;
 
-import main.VaporApp;
-
 import javax.swing.JButton;
 import java.awt.Color;
-import java.awt.Graphics;
+import main.VaporApp;
+import com.loginapp.database.UserShoppingCartDAO;
 
 public class WishlistGameLabel extends BaseGameLabel {
     private final JButton purchaseButton;
     private final JButton wishlistButton;
+
     public WishlistGameLabel(int id, String title, String developer, String publisher,
                              String genre, String tags, String releaseDate, String description,
                              float price, String platforms, float avgRating) {
         super(id, title, developer, publisher, genre, tags, releaseDate, description, price, platforms, avgRating);
-        add(purchaseButton = new JButton() {{
+
+        add(purchaseButton = new JButton("Add to Cart") {{
+            setBackground(Color.GREEN);
             addActionListener(e -> {
-                // SQL: add/remove item to/from cart
-                 VaporApp.APP_SINGLETON.refreshCart();
+                UserShoppingCartDAO.addItemToCart(id, 1);  // Assumes quantity is 1
+                VaporApp.APP_SINGLETON.refreshCart(); // Refresh the cart UI after adding item
             });
         }});
 
-        add(wishlistButton = new JButton() {{
-            setText("Remove from Wishlist");
+        add(wishlistButton = new JButton("Remove from Wishlist") {{
             setBackground(Color.RED);
             addActionListener(e -> {
-                // SQL: remove item from wishlist
-                 VaporApp.APP_SINGLETON.refreshWishlist();
+                UserShoppingCartDAO.deleteItemFromWishlist(id); // Remove item from wishlist
+                VaporApp.APP_SINGLETON.refreshWishlist(); // Refresh the wishlist UI
             });
         }});
     }
 
+    @Override
     protected void updateButtons(Graphics g) {
-        if (false) { // SQL: check if game is owned or game is not in wishlist
+        int userId = VaporApp.APP_SINGLETON.getUserId();
+        boolean isOwned = UserShoppingCartDAO.isGameInLibrary(userId, getId()); // Need to implement this method in DAO
+        boolean isInCart = UserShoppingCartDAO.isItemInCart(getId());
+
+        if (isOwned) {
             setVisible(false);
-            // SQL: remove game from wishlist if it is owned
-            VaporApp.APP_SINGLETON.refreshWishlist(); // Refresh wishlist
+            UserShoppingCartDAO.deleteItemFromWishlist(getId()); // If owned, remove from wishlist
+            VaporApp.APP_SINGLETON.refreshWishlist();
             return;
         }
 
         int height = (getHeight() - 60) / 2;
         int width = height * 4;
-        int x = getWidth() - (width) - 20;
+        int x = getWidth() - width - 20;
 
         purchaseButton.setBounds(x, 20, width, height);
         wishlistButton.setBounds(x, 20 + height + 20, width, height);
 
-        if (false) { // SQL: check if game is in cart
+        if (isInCart) {
             purchaseButton.setText("Remove from Cart");
             purchaseButton.setBackground(Color.RED);
         } else {
-            purchaseButton.setText("Add from Cart");
+            purchaseButton.setText("Add to Cart");
             purchaseButton.setBackground(Color.GREEN);
         }
     }
