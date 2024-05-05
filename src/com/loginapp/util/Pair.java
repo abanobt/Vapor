3399976ebb61245999effbc27728e43c15110b69
abanobt <145419255+abanobt@users.noreply.github.com>
2024-05-05 12:09:43 -0400

    Create Pair class
    Used to store two items, like a tuple

---
+++ src/com/loginapp/util/Pair.java
@@ -0,0 +1,11 @@
+package com.loginapp.util;
+
+public class Pair<T,U> {
+    public T firstItem;
+    public U secondItem;
+
+    public Pair(T firstItem, U secondItem) {
+        this.firstItem = firstItem;
+        this.secondItem = secondItem;
+    }
+}
