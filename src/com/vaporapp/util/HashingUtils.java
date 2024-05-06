d6175c29a4daf6a78a46da741a0c0412126a9427
chris-k-njit <ck378@njit.edu>
2024-05-05 22:25:50 -0400

    redid original code.

--- src/com/vaporapp/util/HashingUtils.java
+++ src/com/vaporapp/util/HashingUtils.java
@@ -1,16 +1,34 @@
-package com.loginapp.util;
+package com.vaporapp.util;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+/**
+ * Utility class for password hashing and verification using BCrypt.
+ */
 public class HashingUtils {
 
-    // Method to hash a password
+    /**
+     * Hashes a plain text password using BCrypt with an automatically generated salt.
+     * 
+     * @param password The plain text password to hash.
+     * @return A string containing the hashed password.
+     */
     public static String hashPassword(String password) {
+        // BCrypt.hashpw hashes the password using the bcrypt algorithm and a newly generated salt.
+        // BCrypt.gensalt() generates a salt that is used in the hashing process to ensure uniqueness of the hashes.
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
 
-    // Method to check a password against a hashed password
+    /**
+     * Checks if a plain text password matches a given hashed password.
+     * 
+     * @param password The plain text password to check.
+     * @param hashedPassword The hashed password against which the plain text password is checked.
+     * @return true if the password matches the hashed password, false otherwise.
+     */
     public static boolean checkPassword(String password, String hashedPassword) {
+        // BCrypt.checkpw compares the plain text password with a hashed password.
+        // It returns true if the password, when hashed with the same salt as the hashedPassword, matches the hashedPassword.
         return BCrypt.checkpw(password, hashedPassword);
     }
 }
--- src/com/vaporapp/util/Pair.java
+++ src/com/vaporapp/util/Pair.java
@@ -1,9 +1,25 @@
-package com.loginapp.util;
+package com.vaporapp.util;
 
-public class Pair<T,U> {
+/**
+ * A generic class for pairing two objects together.
+ * This class allows two objects of potentially different types to be treated as a single unit.
+ * It can be useful in scenarios where you need to return two values from a method or when you want
+ * to link two objects together without creating a specific class for them.
+ *
+ * @param <T> the type of the first object in the pair
+ * @param <U> the type of the second object in the pair
+ */
+public class Pair<T, U> {
+    // Public fields to hold the first and second items of the pair.
     public T firstItem;
     public U secondItem;
 
+    /**
+     * Constructs a new Pair with specified first and second items.
+     * 
+     * @param firstItem The first item of the pair, of type T.
+     * @param secondItem The second item of the pair, of type U.
+     */
     public Pair(T firstItem, U secondItem) {
         this.firstItem = firstItem;
         this.secondItem = secondItem;
